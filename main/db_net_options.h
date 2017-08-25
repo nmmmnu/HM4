5d1de248716706f47034aa98c53ec8046a86a8af
Nikolay Mihaylov <nmmm@nmmm.nu>
2017-08-25 17:05:49 +0300

    ongoing

--- main/db_net_options.h
+++ main/db_net_options.h
@@ -18,31 +18,42 @@ public:
 	uint32_t	max_clients		= 512;
 	size_t		max_memlist_size	= 100;
 
+private:
+	constexpr static const char *OPT_immutable		= "immutable"		;
+	constexpr static const char *OPT_db_path        	= "db_path"		;
+
+	constexpr static const char *OPT_host			= "host"		;
+	constexpr static const char *OPT_port			= "port"		;
+	constexpr static const char *OPT_timeout		= "timeout"		;
+
+	constexpr static const char *OPT_max_clients		= "max_clients"		;
+	constexpr static const char *OPT_max_memlist_size	= "max_memlist_size"	;
+
 public:
 	void operator()(const StringRef &name, const StringRef &val){
-		do_(name, val, "immutable",		immutable		);
-		do_(name, val, "db_path",		db_path			);
+		do_(name, val, OPT_immutable,		immutable		);
+		do_(name, val, OPT_db_path,		db_path			);
 
-		do_(name, val, "host",			host			);
-		do_(name, val, "port",			port			);
-		do_(name, val, "timeout",		timeout			);
+		do_(name, val, OPT_host,		host			);
+		do_(name, val, OPT_port,		port			);
+		do_(name, val, OPT_timeout,		timeout			);
 
-		do_(name, val, "max_clients",		max_clients		);
-		do_(name, val, "max_memlist_size",	max_memlist_size	);
+		do_(name, val, OPT_max_clients,		max_clients		);
+		do_(name, val, OPT_max_memlist_size,	max_memlist_size	);
 	}
 
 	static void print(){
-		MyOptions opt;
+		const MyOptions opt;
 
-		print__("immutable",		(int)opt.immutable,	"Start immutable server (1/0)"		);
-		print__("db_path",		opt.db_path,		"Path to database"			);
+		print__(OPT_immutable,		opt.immutable,		"Start immutable server (1/0)"		);
+		print__(OPT_db_path,		opt.db_path,		"Path to database"			);
 
-		print__("host",			opt.host,		"TCP host to listen (not working)"	);
-		print__("port",			opt.port,		"TCP port to listen"			);
-		print__("timeout",		opt.timeout,		"Connection timeout in seconds"		);
+		print__(OPT_host,		opt.host,		"TCP host to listen (not working)"	);
+		print__(OPT_port,		opt.port,		"TCP port to listen"			);
+		print__(OPT_timeout,		opt.timeout,		"Connection timeout in seconds"		);
 
-		print__("max_clients",		opt.max_clients,	"Max Clients"				);
-		print__("max_memlist_size",	opt.max_memlist_size,	"Max size of memlist in MB"		);
+		print__(OPT_max_clients,	opt.max_clients,	"Max Clients"				);
+		print__(OPT_max_memlist_size,	opt.max_memlist_size,	"Max size of memlist in MB"		);
 	}
 
 private:
@@ -67,6 +78,7 @@ private:
 	static void assign_(const StringRef &, nullptr_t){
 		/* nada */
 	}
+
 private:
 	template<class T>
 	static void print__(const char *name, const T &value, const char *description){
@@ -79,8 +91,12 @@ private:
 		;
 	}
 
+	static void print__(const char *name, uint8_t const value, const char *description){
+		print__(name, (int) value, description);
+	}
+
 	static void print__(const char *name, nullptr_t, const char *description){
-		print__(name, "-", description);
+		print__(name, "n/a", description);
 	}
 };
 
