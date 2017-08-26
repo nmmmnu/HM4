70fc24d7e99710e71efcb34b07bee9469e5cdf62
Nikolay Mihaylov <nmmm@nmmm.nu>
2017-08-26 20:54:06 +0300

    refactor MyOptions

--- main/db_net_options.h
+++ main/db_net_options.h
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <array>
 #include <iostream>
 #include <iomanip>
 
@@ -17,63 +18,103 @@ public:
 	size_t		max_memlist_size	= 100;
 
 private:
-	constexpr static const char *OPT_immutable		= "immutable"		;
-	constexpr static const char *OPT_db_path        	= "db_path"		;
+	enum class Option{
+		immutable		,
+		db_path   	    	,
 
-	constexpr static const char *OPT_host			= "host"		;
-	constexpr static const char *OPT_port			= "port"		;
-	constexpr static const char *OPT_timeout		= "timeout"		;
+		host			,
+		port			,
+		timeout			,
 
-	constexpr static const char *OPT_max_clients		= "max_clients"		;
-	constexpr static const char *OPT_max_memlist_size	= "max_memlist_size"	;
+		max_clients		,
+		max_memlist_size
+	};
+
+private:
+	struct OptPair{
+		StringRef	str;
+		Option		opt;
+		const char	*descr;
+
+		bool operator == (const StringRef &s) const{
+			return str == s;
+		}
+	};
+
+private:
+	constexpr static size_t ARRAY_SIZE = 7;
+
+	using OptionsArray = std::array<OptPair, ARRAY_SIZE>;
+
+	constexpr static OptionsArray options{
+		OptPair{ "immutable",		Option::immutable,		"Start immutable server (1/0)"		},
+		OptPair{ "db_path",		Option::db_path,  	    	"Path to database"			},
+
+		OptPair{ "host",		Option::host,			"TCP host to listen (not working)"	},
+		OptPair{ "port",		Option::port,			"TCP port to listen"			},
+		OptPair{ "timeout",		Option::timeout,		"Connection timeout in seconds"	},
+
+		OptPair{ "max_clients",		Option::max_clients,		"Max Clients"				},
+		OptPair{ "max_memlist_size",	Option::max_memlist_size,	"Max size of memlist in MB"		}
+	};
+
+private:
+	void assignOption_(const Option opt, const StringRef &value){
+		switch(opt){
+		case Option::immutable		: return assign_(immutable,		value);
+		case Option::db_path		: return assign_(db_path,  	    	value);
+
+		case Option::host		: return assign_(host,			value);
+		case Option::port		: return assign_(port,			value);
+		case Option::timeout		: return assign_(timeout,		value);
+
+		case Option::max_clients	: return assign_(max_clients,		value);
+		case Option::max_memlist_size	: return assign_(max_memlist_size,	value);
+		}
+	}
 
 public:
-	void operator()(const StringRef &name, const StringRef &val){
-		const INIFileAssignHelper ini{ name, val };
+	void operator()(const StringRef &name, const StringRef &value){
+		const auto &it = std::find(options.begin(), options.end(), name);
 
-		ini.setOption(OPT_immutable,		immutable		);
-		ini.setOption(OPT_db_path,		db_path			);
-
-		ini.setOption(OPT_host,			host			);
-		ini.setOption(OPT_port,			port			);
-		ini.setOption(OPT_timeout,		timeout			);
-
-		ini.setOption(OPT_max_clients,		max_clients		);
-		ini.setOption(OPT_max_memlist_size,	max_memlist_size	);
+		if (it != options.end())
+			return assignOption_(it->opt, value);
 	}
 
 	static void print(){
-		const MyOptions opt;
-
-		print__(OPT_immutable,		opt.immutable,		"Start immutable server (1/0)"		);
-		print__(OPT_db_path,		opt.db_path,		"Path to database"			);
-
-		print__(OPT_host,		opt.host,		"TCP host to listen (not working)"	);
-		print__(OPT_port,		opt.port,		"TCP port to listen"			);
-		print__(OPT_timeout,		opt.timeout,		"Connection timeout in seconds"		);
-
-		print__(OPT_max_clients,	opt.max_clients,	"Max Clients"				);
-		print__(OPT_max_memlist_size,	opt.max_memlist_size,	"Max size of memlist in MB"		);
+		for(const auto &o : options){
+			// std::setw() is broken
+			print__(o.str.c_str(), o.descr);
+		}
 	}
 
 private:
 	template<class T>
-	static void print__(const char *name, const T &value, const char *description){
+	static void assign_(T &param, const StringRef &value){
+		static_assert(std::is_unsigned<T>::value, "T must be unsigned type");
+
+		auto const default_value = param;
+
+		param = stou<T>(value, default_value);
+	}
+
+	static void assign_(std::string &param, const StringRef &value){
+		param = value;
+	}
+
+	static void assign_(nullptr_t, const StringRef &){
+		/* nada */
+	}
+
+private:
+	static void print__(const char *name, const char *description){
 		std::cout
 			<< '\t'
 			<< std::setw(20) << std::left << name
-			<< std::setw(6) << std::left << value
 			<< description
 			<< '\n'
 		;
 	}
-
-	static void print__(const char *name, uint8_t const value, const char *description){
-		print__(name, int{ value }, description);
-	}
-
-	static void print__(const char *name, nullptr_t, const char *description){
-		print__(name, "n/a", description);
-	}
 };
 
+constexpr MyOptions::OptionsArray MyOptions::options;
