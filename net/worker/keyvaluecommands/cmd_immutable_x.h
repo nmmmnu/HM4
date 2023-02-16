41d80563c4416d293db929057acc3a04326a06eb
Nikolay Mihaylov <nmmm@nmmm.nu>
2023-02-16 15:06:45 +0200

    new command HGETKEYS, HGETVALS

--- net/worker/keyvaluecommands/cmd_immutable_x.h
+++ net/worker/keyvaluecommands/cmd_immutable_x.h
@@ -13,7 +13,41 @@ namespace net::worker::commands::ImmutableX{
 
 
 
-		template<bool Tail, class It, class Container, class ProjectionKey>
+		enum class AccumulateOutput{
+			KEYS,
+			VALS,
+			BOTH,
+			BOTH_WITH_TAIL
+		};
+
+		#if 0
+		template<class It, class ProjectionKey>
+		void accumulateResults(std::string_view const prefix, It it, It eit, ProjectionKey projKey){
+			uint32_t iterations	= 0;
+			uint32_t results	= 0;
+
+			for(;it != eit;++it){
+				auto const &key = it->getKey();
+
+				auto pkey = projKey(key);
+
+				if (++iterations > ITERATIONS)
+					return results;
+
+				if (! prefix.empty() && ! same_prefix(prefix, key))
+					return results;
+
+				if (! it->isValid(std::true_type()))
+					continue;
+
+				++results;
+			}
+
+			return results;
+		}
+		#endif
+
+		template<AccumulateOutput Out, class It, class Container, class ProjectionKey>
 		void accumulateResults(uint32_t const maxResults, std::string_view const prefix, It it, It eit, Container &container, ProjectionKey projKey){
 			uint32_t iterations	= 0;
 			uint32_t results	= 0;
@@ -26,7 +60,7 @@ namespace net::worker::commands::ImmutableX{
 				auto pkey = projKey(key);
 
 				if (++iterations > ITERATIONS){
-					if constexpr(Tail)
+					if constexpr(Out == AccumulateOutput::BOTH_WITH_TAIL)
 						container.emplace_back(pkey);
 
 					return;
@@ -41,20 +75,25 @@ namespace net::worker::commands::ImmutableX{
 					continue;
 
 				if (++results > maxResults){
-					container.emplace_back(pkey);
+					if constexpr(Out == AccumulateOutput::BOTH_WITH_TAIL)
+						container.emplace_back(pkey);
+
 					return;
 				}
 
 				auto const &val = it->getVal();
 
-				container.emplace_back(pkey);
-				container.emplace_back(val);
+				if constexpr(Out == AccumulateOutput::BOTH_WITH_TAIL || Out == AccumulateOutput::BOTH || Out == AccumulateOutput::KEYS)
+					container.emplace_back(pkey);
+
+				if constexpr(Out == AccumulateOutput::BOTH_WITH_TAIL || Out == AccumulateOutput::BOTH || Out == AccumulateOutput::VALS)
+					container.emplace_back(val);
 			}
 		}
 
-		template<class It, class Container>
+		template<AccumulateOutput Out, class It, class Container>
 		void accumulateResults(uint32_t const maxResults, std::string_view const prefix, It it, It eit, Container &container){
-			return accumulateResults<true>(maxResults, prefix, it, eit, container, [](auto x){
+			return accumulateResults<Out>(maxResults, prefix, it, eit, container, [](auto x){
 				return x;
 			});
 		}
@@ -99,7 +138,7 @@ namespace net::worker::commands::ImmutableX{
 			auto const count   = myClamp( from_string<uint64_t>(p[2]) );
 			auto const &prefix = p[3];
 
-			accumulateResults(
+			accumulateResults<AccumulateOutput::BOTH_WITH_TAIL>(
 				count					,
 				prefix					,
 				db->find(key, std::false_type{})	,
@@ -163,9 +202,7 @@ namespace net::worker::commands::ImmutableX{
 					return x;
 			};
 
-			bool const no_tail = false;
-
-			accumulateResults<no_tail>(
+			accumulateResults<AccumulateOutput::BOTH>(
 				ITERATIONS				,
 				key					,
 				db->find(key, std::false_type{})	,
@@ -185,14 +222,136 @@ namespace net::worker::commands::ImmutableX{
 
 
 
+	template<class Protocol, class DBAdapter>
+	struct HGETKEYS : BaseRO<Protocol,DBAdapter>{
+		const std::string_view *begin() const final{
+			return std::begin(cmd);
+		};
+
+		const std::string_view *end()   const final{
+			return std::end(cmd);
+		};
+
+		constexpr static std::size_t MAX_KEY_SIZE = hm4::PairConf::MAX_KEY_SIZE
+						- DBAdapter::SEPARATOR.size()
+						- 16;
+
+		void process(ParamContainer const &p, DBAdapter &db, Result<Protocol> &result, OutputBlob &blob) final{
+			if (p.size() != 2)
+				return;
+
+
+
+			using namespace getx_impl_;
+
+			static_assert(OutputBlob::ContainerSize >= 2 * ITERATIONS + 1);
+
+
+
+			auto const &keyN = p[1];
+
+			if (keyN.empty())
+				return;
+
+			if (keyN.size() > MAX_KEY_SIZE)
+				return;
+
+			auto const key = concatenateBuffer(blob.buffer_key, keyN, DBAdapter::SEPARATOR);
+
+			auto const key_size = key.size();
+
+			auto proj = [key_size](std::string_view x) -> std::string_view{
+				if (key_size <= x.size())
+					return x.substr(key_size);
+				else
+					return x;
+			};
+
+			accumulateResults<AccumulateOutput::KEYS>(
+				ITERATIONS				,
+				key					,
+				db->find(key, std::false_type{})	,
+				std::end(*db)				,
+				blob.container				,
+				proj
+			);
+
+			return result.set_container(blob.container);
+		}
+
+	private:
+		constexpr inline static std::string_view cmd[]	= {
+			"hgetkeys",	"HGETKEYS"
+		};
+	};
+
+
+
+	template<class Protocol, class DBAdapter>
+	struct HGETVALS : BaseRO<Protocol,DBAdapter>{
+		const std::string_view *begin() const final{
+			return std::begin(cmd);
+		};
+
+		const std::string_view *end()   const final{
+			return std::end(cmd);
+		};
+
+		constexpr static std::size_t MAX_KEY_SIZE = hm4::PairConf::MAX_KEY_SIZE
+						- DBAdapter::SEPARATOR.size()
+						- 16;
+
+		void process(ParamContainer const &p, DBAdapter &db, Result<Protocol> &result, OutputBlob &blob) final{
+			if (p.size() != 2)
+				return;
+
+
+
+			using namespace getx_impl_;
+
+			static_assert(OutputBlob::ContainerSize >= 2 * ITERATIONS + 1);
+
+
+
+			auto const &keyN = p[1];
+
+			if (keyN.empty())
+				return;
+
+			if (keyN.size() > MAX_KEY_SIZE)
+				return;
+
+			auto const key = concatenateBuffer(blob.buffer_key, keyN, DBAdapter::SEPARATOR);
+
+			accumulateResults<AccumulateOutput::VALS>(
+				ITERATIONS				,
+				key					,
+				db->find(key, std::false_type{})	,
+				std::end(*db)				,
+				blob.container
+			);
+
+			return result.set_container(blob.container);
+		}
+
+	private:
+		constexpr inline static std::string_view cmd[]	= {
+			"hgetvals",	"HGETVALS"
+		};
+	};
+
+
+
 	template<class Protocol, class DBAdapter, class RegisterPack>
 	struct RegisterModule{
 		constexpr inline static std::string_view name	= "getx";
 
 		static void load(RegisterPack &pack){
 			return registerCommands<Protocol, DBAdapter, RegisterPack,
-				GETX	,
-				HGETALL
+				GETX		,
+				HGETALL		,
+				HGETKEYS	,
+				HGETVALS
 			>(pack);
 		}
 	};
