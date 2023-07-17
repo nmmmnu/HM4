2d048b3d20fc4421ee2aa4a7ffbf3a8470818f41
Nikolay Mihaylov <nmmm@nmmm.nu>
2023-07-17 22:40:10 +0300

    logger

--- common/logger.cc
+++ common/logger.cc
@@ -1,22 +1,60 @@
 #include "logger.h"
 #include "mytime.h"
 
-Logger g_log;
 
-Logger &getLoggerSingleton(){
-	return g_log;
-}
 
-void Logger::LoggerStream::outputBanner_(const char *banner){
-	if (writting_){
-		using namespace mytime;
+namespace my_logger{
+	Logger g_log;
 
-		to_string_buffer_t buffer;
+	void Logger::LoggerStream::outputBanner_(const char *banner){
+		if (writting_){
+			using namespace mytime;
 
-		os_
-			<< toString(now(), TIME_FORMAT_STANDARD, buffer)
-			<< ' ' << banner << ' '
-		;
+			to_string_buffer_t buffer;
+
+			os_
+				<< toString(now(), TIME_FORMAT_STANDARD, buffer)
+				<< ' ' << banner << ' '
+			;
+		}
 	}
+
 }
+
+my_logger::Logger &getLoggerSingleton(){
+	return my_logger::g_log;
+}
+
+
+#if 0
+		[[nodiscard]]
+		auto startup() const{
+			return get(Level::STARTUP	);
+		}
+
+		[[nodiscard]]
+		auto fatal() const{
+			return get(Level::FATAL		);
+		}
+
+		[[nodiscard]]
+		auto error() const{
+			return get(Level::ERROR		);
+		}
+
+		[[nodiscard]]
+		auto warning() const{
+			return get(Level::WARNING	);
+		}
+
+		[[nodiscard]]
+		auto notice() const{
+			return get(Level::NOTICE	);
+		}
+
+		[[nodiscard]]
+		auto debug() const{
+			return get(Level::DEBUG		);
+		}
+#endif
 
--- common/logger.h
+++ common/logger.h
@@ -6,156 +6,145 @@
 #include "fmt/core.h"
 #include "fmt/ostream.h"
 
+namespace my_logger{
+
+	struct Logger{
+		enum class Level : unsigned{
+			STARTUP	= 0,
+			FATAL	= 1,
+			ERROR	= 2,
+			WARNING	= 3,
+			NOTICE	= 4,
+			DEBUG	= 5
+		};
+
+		constexpr static auto DEFAULT_LEVEL = Level::NOTICE;
+
+		Logger(Level level = DEFAULT_LEVEL) : level_(level){}
+
+	private:
+		/*
+			Black	\033[30m	\033[40m
+			Red	\033[31m	\033[41m
+			Green	\033[32m	\033[42m
+			Yellow	\033[33m	\033[43m
+			Blue	\033[34m	\033[44m
+			Purple	\033[35m	\033[45m
+			Cyan	\033[36m	\033[46m
+			White	\033[37m	\033[47m
+
+			Bold	\033[1m
+			Reset	\033[0m
+		*/
+
+		constexpr static inline const char *banner[]{
+			"\033[1;37;42m"	"[STARTUP]"	"\033[0m",
+			"\033[1;37;41m"	"[ FATAL ]"	"\033[0m",
+			"\033[1;31m"	"[ ERROR ]"	"\033[0m",
+			"\033[1;33m"	"[WARNING]"	"\033[0m",
+			"\033[32m"	"[NOTICES]"	"\033[0m",
+			"\033[36m"	"[ DEBUG ]"	"\033[0m"
+		};
+
+	public:
+		[[nodiscard]]
+		auto get(Level level) const{
+			bool const writting = level <= level_;
+
+			return LoggerStream(banner[static_cast<unsigned>(level)], writting);
+		}
+
+		template<Level level>
+		[[nodiscard]]
+		auto get() const{
+			return get(level	);
+		}
+
+		[[nodiscard]]
+		auto getLevel() const{
+			return level_;
+		}
+
+		void setLevel(Level level){
+			level_ = level;
+		}
+
+		void setLevel(unsigned level){
+			level_ = [level](){
+				using _ = Level;
+
+				switch(level){
+				case 0:  return _::STARTUP	;
+				case 1:  return _::FATAL	;
+				case 2:  return _::ERROR	;
+				case 3:  return _::WARNING	;
+				case 4:  return _::NOTICE	;
+				case 5:  return _::DEBUG	;
+				};
+
+				return DEFAULT_LEVEL;
+			}();
+		}
+
+	private:
+		struct LoggerStream{
+			LoggerStream(const char *banner, bool writting, std::ostream &os = std::clog) :
+						writting_	(writting	),
+						os_		(os		){
+
+				outputBanner_(banner);
+			}
+
+			~LoggerStream(){
+				if (writting_)
+					os_ << '\n';
+			}
+
+			template<typename T>
+			auto &operator <<(T const &a) const{
+				if (writting_)
+					os_ << a << ' ';
+
+				return *this;
+			}
+
+			template<typename ...Args>
+			void fmt(Args &&...args){
+				if (writting_)
+					fmt::print(os_, std::forward<Args>(args)...);
+			}
+
+		private:
+			void outputBanner_(const char *banner);
+
+		private:
+			bool		writting_;
+			std::ostream	&os_;
+		};
+
+	private:
+		Level	level_;
+	};
+
+} // namespace my_logger
+
+
+
 struct Logger{
-	enum Level{
-		STARTUP	= 0,
-		FATAL	= 1,
-		ERROR	= 2,
-		WARNING	= 3,
-		NOTICE	= 4,
-		DEBUG	= 5
-	};
+	using Level = my_logger::Logger::Level;
 
-	constexpr static Level DEFAULT_LEVEL = NOTICE;
-
-	Logger(Level level = DEBUG) : level_(level){}
-
-private:
-	/*
-		Black	\033[30m	\033[40m
-		Red	\033[31m	\033[41m
-		Green	\033[32m	\033[42m
-		Yellow	\033[33m	\033[43m
-		Blue	\033[34m	\033[44m
-		Purple	\033[35m	\033[45m
-		Cyan	\033[36m	\033[46m
-		White	\033[37m	\033[47m
-
-		Bold	\033[1m
-		Reset	\033[0m
-	*/
-
-	constexpr static inline const char *banner[]{
-		"\033[1;37;42m"	"[STARTUP]"	"\033[0m",
-		"\033[1;37;41m"	"[ FATAL ]"	"\033[0m",
-		"\033[1;31m"	"[ ERROR ]"	"\033[0m",
-		"\033[1;33m"	"[WARNING]"	"\033[0m",
-		"\033[32m"	"[NOTICES]"	"\033[0m",
-		"\033[36m"	"[ DEBUG ]"	"\033[0m"
-	};
-
-public:
-	[[nodiscard]]
-	auto get(Level level) const{
-		bool const writting = level <= level_;
-
-		return LoggerStream(banner[level], writting);
-	}
-
-	template<Level level>
-	[[nodiscard]]
-	auto get() const{
-		return get(level	);
-	}
-
-	[[nodiscard]]
-	auto startup() const{
-		return get(STARTUP	);
-	}
-
-	[[nodiscard]]
-	auto fatal() const{
-		return get(FATAL	);
-	}
-
-	[[nodiscard]]
-	auto error() const{
-		return get(ERROR	);
-	}
-
-	[[nodiscard]]
-	auto warning() const{
-		return get(WARNING	);
-	}
-
-	[[nodiscard]]
-	auto notice() const{
-		return get(NOTICE	);
-	}
-
-	[[nodiscard]]
-	auto debug() const{
-		return get(DEBUG	);
-	}
-
-	[[nodiscard]]
-	auto getLevel() const{
-		return level_;
-	}
-
-	void setLevel(Level level){
-		level_ = level;
-	}
-
-	void setLevel(unsigned level){
-		level_ = [level](){
-			switch(level){
-			case 0:  return STARTUP	;
-			case 1:  return FATAL	;
-			case 2:  return ERROR	;
-			case 3:  return WARNING	;
-			case 4:  return NOTICE	;
-			case 5:  return DEBUG	;
-			};
-
-			return DEFAULT_LEVEL;
-		}();
-	}
-
-private:
-	struct LoggerStream{
-		LoggerStream(const char *banner, bool writting, std::ostream &os = std::clog) :
-					writting_	(writting	),
-					os_		(os		){
-
-			outputBanner_(banner);
-		}
-
-		~LoggerStream(){
-			if (writting_)
-				os_ << '\n';
-		}
-
-		template<typename T>
-		auto &operator <<(T const &a) const{
-			if (writting_)
-				os_ << a << ' ';
-
-			return *this;
-		}
-
-		template<typename ...Args>
-		void fmt(Args &&...args){
-			if (writting_)
-				fmt::print(os_, std::forward<Args>(args)...);
-		}
-
-	private:
-		void outputBanner_(const char *banner);
-
-	private:
-		bool		writting_;
-		std::ostream	&os_;
-	};
-
-private:
-	Level	level_;
+	constexpr static auto STARTUP	= Level::STARTUP	;
+	constexpr static auto FATAL	= Level::FATAL		;
+	constexpr static auto ERROR	= Level::ERROR		;
+	constexpr static auto WARNING	= Level::WARNING	;
+	constexpr static auto NOTICE	= Level::NOTICE		;
+	constexpr static auto DEBUG	= Level::DEBUG		;
 };
 
 
 
 [[nodiscard]]
-Logger &getLoggerSingleton();
+my_logger::Logger &getLoggerSingleton();
 
 template<Logger::Level level>
 [[nodiscard]]
