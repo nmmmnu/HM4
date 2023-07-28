87c63adfa2a7268fa24672d7c998ac7b2d432874
Nikolay Mihaylov <nmmm@nmmm.nu>
2023-07-28 22:39:08 +0300

    make VectorList to use pointer_iterator

--- hm4/vectorlist.h
+++ hm4/vectorlist.h
@@ -4,6 +4,8 @@
 #include "ilist.h"
 #include "listcounter.h"
 
+#include "pointer_iterator.h"
+
 #include "mynarrow.h"
 
 #include <vector>
@@ -13,7 +15,6 @@ namespace hm4{
 template<class T_Allocator>
 class VectorList{
 	using OVector	= std::vector<Pair *>;
-	using OVectorIt	= OVector::const_iterator;
 
 public:
 	using Allocator		= T_Allocator;
@@ -21,7 +22,7 @@ public:
 	using difference_type	= config::difference_type;
 
 public:
-	class iterator;
+	using iterator = pointer_iterator<OVector::const_iterator>;
 
 public:
 	VectorList(Allocator &allocator) : allocator_(& allocator){}
@@ -82,136 +83,15 @@ public:
 public:
 	template<bool B>
 	iterator find(std::string_view const key, std::bool_constant<B> exact) const;
-	iterator begin() const noexcept;
-	iterator end() const noexcept;
+
+	iterator begin() const noexcept{
+		return std::begin(vector_);
+	}
+
+	iterator end() const noexcept{
+		return std::end(vector_);
+	}
 };
-
-// ==============================
-
-template<class T_Allocator>
-class VectorList<T_Allocator>::iterator{
-public:
-	constexpr iterator(OVectorIt const &it) : ptr(it){}
-	constexpr iterator(OVectorIt &&it) : ptr(std::move(it)){}
-
-public:
-	using difference_type	= VectorList::difference_type;
-	using value_type	= const Pair;
-	using pointer		= value_type *;
-	using reference		= value_type &;
-	using iterator_category	= std::random_access_iterator_tag;
-
-public:
-	// increment / decrement
-	iterator &operator++(){
-		++ptr;
-		return *this;
-	}
-
-	iterator &operator--(){
-		--ptr;
-		return *this;
-	}
-
-	iterator operator++(int){
-		auto tmp = ptr;
-		++ptr;
-		return { tmp };
-	}
-
-	iterator operator--(int){
-		auto tmp = ptr;
-		--ptr;
-		return { tmp };
-	}
-
-public:
-	// arithmetic
-	// https://www.boost.org/doc/libs/1_50_0/boost/container/vector.hpp
-
-	iterator& operator+=(difference_type const off){
-		ptr += off;
-		return *this;
-	}
-
-	iterator operator +(difference_type const off) const{
-		return { ptr + off };
-	}
-
-	iterator& operator-=(difference_type const off){
-		ptr -= off;
-		return *this;
-	}
-
-	iterator operator -(difference_type const off) const{
-		return { ptr - off };
-	}
-
-	friend iterator operator +(difference_type const  off, iterator const &it){
-		return it.ptr + off;
-	}
-
-	difference_type operator -(iterator const &other) const{
-		return ptr - other.ptr;
-	}
-
-public:
-	// compare
-	bool operator==(iterator const &other) const{
-		return ptr == other.ptr;
-	}
-
-	bool operator!=(iterator const &other) const{
-		return ptr != other.ptr;
-	}
-
-	bool operator >(iterator const &other) const{
-		return ptr >  other.ptr;
-	}
-
-	bool operator>=(iterator const &other) const{
-		return ptr >= other.ptr;
-	}
-
-	bool operator <(iterator const &other) const{
-		return ptr <  other.ptr;
-	}
-
-	bool operator<=(iterator const &other) const{
-		return ptr <= other.ptr;
-	}
-
-public:
-	// dereference
-	reference operator*() const{
-		return **ptr;
-	}
-
-	pointer operator->() const{
-		return & operator*();
-	}
-
-	reference operator[](difference_type const off) const{
-		return *ptr[off];
-	}
-
-private:
-	OVectorIt	ptr;
-};
-
-// ==============================
-
-template<class T_Allocator>
-inline auto VectorList<T_Allocator>::begin() const noexcept -> iterator{
-	return std::begin(vector_);
-}
-
-template<class T_Allocator>
-inline auto VectorList<T_Allocator>::end() const noexcept -> iterator{
-	return std::end(vector_);
-}
-
-
 
 } // namespace
 
