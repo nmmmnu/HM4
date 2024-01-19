06f9f1159e08ae94758360395fdc45dd8fd1eb3d
Nikolay Mihaylov <nmmm@nmmm.nu>
2024-01-19 16:26:36 +0200

    morton curve set / del

--- net/worker/keyvaluecommands/cmd_mortoncurve.h
+++ net/worker/keyvaluecommands/cmd_mortoncurve.h
@@ -10,20 +10,31 @@ namespace net::worker::commands::MortonCurve{
 
 		using MC2Buffer = std::array<char, subNSize>;
 
-		constexpr bool isMC2KeyValid(size_t keyN){
-			return hm4::Pair::isKeyValid(keyN + subNSize + 1 + 16);
-		}
-
 		constexpr bool isMC2KeyValid(std::string_view key){
-			return isMC2KeyValid(key.size());
+			return hm4::Pair::isKeyValid(key.size() + subNSize + 1 + 16);
 		}
 
-		constexpr std::string_view toHex(uint64_t const x, MC2Buffer &buffer){
+		constexpr std::string_view toHex(uint64_t const z, MC2Buffer &buffer){
 			using namespace hex_convert;
 
 			constexpr auto opt = options::lowercase | options::nonterminate;
 
-			return hex_convert::toHex<uint64_t, opt>(x, buffer.data());
+			return hex_convert::toHex<uint64_t, opt>(z, buffer.data());
+		}
+
+		constexpr std::string_view toHex(uint32_t const x, uint32_t const y, MC2Buffer &buffer){
+			return toHex(
+				morton_curve::toMorton2D(x, y),
+				buffer
+			);
+		}
+
+		inline std::string_view toHex(std::string_view const x, std::string_view const y, MC2Buffer &buffer){
+			return toHex(
+				from_string<uint32_t>(x),
+				from_string<uint32_t>(y),
+				buffer
+			);
 		}
 	}
 
@@ -55,18 +66,14 @@ namespace net::worker::commands::MortonCurve{
 			if (!isMC2KeyValid(keyN))
 				return result.set_error(ResultErrorMessages::INVALID_KEY_SIZE);
 
-			auto const x = from_string<uint32_t>(p[2]);
-			auto const y = from_string<uint32_t>(p[3]);
-
-			auto const z = morton_curve::toMorton2D(x, y);
+			auto const &x = p[2];
+			auto const &y = p[3];
 
 			MC2Buffer buffer;
 
-			auto const subN = toHex(z, buffer);
+			auto const subN = toHex(x, y, buffer);
 
 			auto const key = concatenateBuffer(blob.buffer_key, keyN, DBAdapter::SEPARATOR, subN);
-
-			logger<Logger::DEBUG>() << "Morton" << x << y << z << subN << key;
 
 			return result.set(
 				hm4::getPairVal(*db, key)
@@ -104,24 +111,23 @@ namespace net::worker::commands::MortonCurve{
 			if (keyN.empty())
 				return result.set_error(ResultErrorMessages::EMPTY_KEY);
 
+			if (!isMC2KeyValid(keyN))
+				return result.set_error(ResultErrorMessages::INVALID_KEY_SIZE);
+
 			auto &container = blob.container;
 			container.clear();
 
 			auto const varg = 2;
 
 			for(auto itk = std::begin(p) + varg; itk != std::end(p); itk += 2){
-				auto const x = from_string<uint32_t>(*itk);
-				auto const y = from_string<uint32_t>(*std::next(itk));
-
-				auto const z = morton_curve::toMorton2D(x, y);
+				auto const &x = *itk;
+				auto const &y = *std::next(itk);
 
 				MC2Buffer buffer;
 
-				auto const subN = toHex(z, buffer);
+				auto const subN = toHex(x, y, buffer);
 
 				auto const key = concatenateBuffer(blob.buffer_key, keyN, DBAdapter::SEPARATOR, subN);
-
-				logger<Logger::DEBUG>() << "Morton" << x << y << z << subN << key;
 
 				container.emplace_back(
 					hm4::getPairVal(*db, key)
@@ -163,18 +169,14 @@ namespace net::worker::commands::MortonCurve{
 			if (!isMC2KeyValid(keyN))
 				return result.set_error(ResultErrorMessages::INVALID_KEY_SIZE);
 
-			auto const x = from_string<uint32_t>(p[2]);
-			auto const y = from_string<uint32_t>(p[3]);
-
-			auto const z = morton_curve::toMorton2D(x, y);
+			auto const &x = p[2];
+			auto const &y = p[3];
 
 			MC2Buffer buffer;
 
-			auto const subN = toHex(z, buffer);
+			auto const subN = toHex(x, y, buffer);
 
 			auto const key = concatenateBuffer(blob.buffer_key, keyN, DBAdapter::SEPARATOR, subN);
-
-			logger<Logger::DEBUG>() << "Morton" << x << y << z << subN << key;
 
 			return result.set(
 				hm4::getPairOK(*db, key)
@@ -189,17 +191,129 @@ namespace net::worker::commands::MortonCurve{
 
 
 
+	template<class Protocol, class DBAdapter>
+	struct MC2SET : BaseRW<Protocol,DBAdapter>{
+		const std::string_view *begin() const final{
+			return std::begin(cmd);
+		};
+
+		const std::string_view *end()   const final{
+			return std::end(cmd);
+		};
+
+		// MC2SET a x0 y0 val0 x1 y1 val1
+
+		void process(ParamContainer const &p, DBAdapter &db, Result<Protocol> &result, OutputBlob &blob) final{
+			using namespace morton_curve_impl_;
+
+			if (p.size() < 4 || (p.size() - 2) % 3 != 0)
+				return result.set_error(ResultErrorMessages::NEED_GROUP_PARAMS_4);
+
+			const auto &keyN = p[1];
+
+			if (keyN.empty())
+				return result.set_error(ResultErrorMessages::EMPTY_KEY);
+
+			if (!isMC2KeyValid(keyN))
+				return result.set_error(ResultErrorMessages::INVALID_KEY_SIZE);
+
+			auto const varg = 2;
+
+			for(auto itk = std::begin(p) + varg; itk != std::end(p); itk += 3){
+				if (auto const &val = *(itk + 2); !hm4::Pair::isValValid(val))
+					return result.set_error(ResultErrorMessages::EMPTY_VAL);
+			}
+
+			for(auto itk = std::begin(p) + varg; itk != std::end(p); itk += 3){
+				auto const &x = *itk;
+				auto const &y = *std::next(itk);
+
+				MC2Buffer buffer;
+
+				auto const subN = toHex(x, y, buffer);
+
+				auto const key = concatenateBuffer(blob.buffer_key, keyN, DBAdapter::SEPARATOR, subN);
+
+				auto const &val = *(itk + 2);
+
+				hm4::insert(*db, key, val);
+			}
+
+			return result.set();
+		}
+
+	private:
+		constexpr inline static std::string_view cmd[]	= {
+			"mc2set",		"MC2SET"	,
+			"mc2mset",		"MC2MSET"
+		};
+	};
+
+
+
+	template<class Protocol, class DBAdapter>
+	struct MC2DEL : BaseRW<Protocol,DBAdapter>{
+		const std::string_view *begin() const final{
+			return std::begin(cmd);
+		};
+
+		const std::string_view *end()   const final{
+			return std::end(cmd);
+		};
+
+		// MC2DEL a x0 y0 x1 y1
+
+		void process(ParamContainer const &p, DBAdapter &db, Result<Protocol> &result, OutputBlob &blob) final{
+			using namespace morton_curve_impl_;
+
+			if (p.size() < 4 && p.size() % 2 == 0)
+				return result.set_error(ResultErrorMessages::NEED_GROUP_PARAMS_4);
+
+			const auto &keyN = p[1];
+
+			if (keyN.empty())
+				return result.set_error(ResultErrorMessages::EMPTY_KEY);
+
+			if (!isMC2KeyValid(keyN))
+				return result.set_error(ResultErrorMessages::INVALID_KEY_SIZE);
+
+			auto const varg = 2;
+
+			for(auto itk = std::begin(p) + varg; itk != std::end(p); itk += 2){
+				auto const &x = *itk;
+				auto const &y = *std::next(itk);
+
+				MC2Buffer buffer;
+
+				auto const subN = toHex(x, y, buffer);
+
+				auto const key = concatenateBuffer(blob.buffer_key, keyN, DBAdapter::SEPARATOR, subN);
+
+				hm4::erase(*db, key);
+			}
+
+			return result.set_1();
+		}
+
+	private:
+		constexpr inline static std::string_view cmd[]	= {
+			"mc2del",		"MC2DEL"
+		};
+	};
+
+
+
 	template<class Protocol, class DBAdapter, class RegisterPack>
 	struct RegisterModule{
-		constexpr inline static std::string_view name	= "immutable";
+		constexpr inline static std::string_view name	= "morton_curve";
 
 		static void load(RegisterPack &pack){
 			return registerCommands<Protocol, DBAdapter, RegisterPack,
 				MC2GET		,
 				MC2MGET		,
-				MC2EXISTS	/*,
+				MC2EXISTS	,
 				MC2SET		,
-				MC2DEL	*/
+				MC2DEL
 			>(pack);
 		}
 	};
--- todo.md
+++ todo.md
@@ -2,3 +2,4 @@
 
 -	Fix double work in finding inside PairVector, when we know data is not last element...
 -	Make HGET / GLEN etc to give proper error when counter is exausted.
+-	HMSET documentation and eventually test
