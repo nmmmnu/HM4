6bfd11d3392e9aa2b7c5090636fb80aaf3b9e11a
Nikolay Mihaylov <nmmm@nmmm.nu>
2025-07-14 00:43:23 +0300

    increase version

--- hm4/version.h
+++ hm4/version.h
@@ -3,12 +3,12 @@
 
 namespace hm4{
 	namespace version{
-		constexpr const char *str = "1.3.10.2";
+		constexpr const char *str = "1.3.11.0";
 
 		constexpr int major	= 1;
 		constexpr int minor	= 3;
-		constexpr int revision	= 10;
-		constexpr int build	= 2;
+		constexpr int revision	= 11;
+		constexpr int build	= 0;
 
 		constexpr int num	=	major		* 100000	+
 						minor		* 1000		+
