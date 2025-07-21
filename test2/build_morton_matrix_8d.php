d1c5f7f5e10dff97611d3326f93e5c310a1470fe
Nikolay Mihaylov <nmmm@nmmm.nu>
2025-07-21 12:03:36 +0300

    reduce build_morton_matrix_8d.php to 16M vectors

--- test2/build_morton_matrix_8d.php
+++ test2/build_morton_matrix_8d.php
@@ -18,14 +18,14 @@ $redis->connect("127.0.0.1");
 
 
 
-$max = 10;
+$max = 8;
 
 for($v00 = 0; $v00 < $max; ++$v00)
 for($v01 = 0; $v01 < $max; ++$v01)
 for($v02 = 0; $v02 < $max; ++$v02)
-for($v03 = 0; $v03 < $max; ++$v03)
+for($v03 = 0; $v03 < $max; ++$v03){	printf("%3d : %3d : %3d : %3d of %5d\r", $v00, $v01, $v02, $v03, $max);
 for($v04 = 0; $v04 < $max; ++$v04)
-for($v05 = 0; $v05 < $max; ++$v05){	printf("%3d : %3d : %3d : %3d : %3d : %3d of %5d\r", $v00, $v01, $v02, $v03, $v04, $v05, $max);
+for($v05 = 0; $v05 < $max; ++$v05)
 for($v06 = 0; $v06 < $max; ++$v06)
 for($v07 = 0; $v07 < $max; ++$v07){
 		$sub = "aaa.$v00.$v01.$v02.$v03.$v04.$v05.$v06.$v07";
