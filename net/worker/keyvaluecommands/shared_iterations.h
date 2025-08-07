15da25afec5eaf143476efae768b3a08d1f48e12
Nikolay Mihaylov <nmmm@nmmm.nu>
2025-08-07 12:46:13 +0300

    increased ITERATIONS_LOOPS_MAX

--- net/worker/keyvaluecommands/shared_iterations.h
+++ net/worker/keyvaluecommands/shared_iterations.h
@@ -11,9 +11,10 @@ namespace net::worker::shared::config{
 	constexpr uint32_t ITERATIONS_RESULTS_MAX	= (OutputBlob::ContainerSize - 1) / 2;
 
 	// This has nothing to do with the container size,
-	// but to be unified with cmd_mutable.h
+	// but before we choosed it to be the same as OutputBlob::ContainerSize
+
 	constexpr uint32_t ITERATIONS_LOOPS_MIN		= ITERATIONS_RESULTS_MIN;
-	constexpr uint32_t ITERATIONS_LOOPS_MAX		= OutputBlob::ContainerSize;
+	constexpr uint32_t ITERATIONS_LOOPS_MAX		= 100'000;
 
 	constexpr uint32_t ITERATIONS_IDLE		= 10;
 
