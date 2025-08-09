79abe11d27003a47483c19b385ecad6e955f26e5
Nikolay Mihaylov <nmmm@nmmm.nu>
2025-08-09 18:08:37 +0300

    remove StaticBuffer, since functionality is almost the same with StaticMemoryResource

--- include/staticbuffer.h
+++ include/staticbuffer.h
@@ -6,64 +6,6 @@
 
 namespace MyBuffer{
 
-	template<typename T, std::size_t Size>
-	struct StaticBuffer{
-		using value_type	= T;
-		using size_type		= std::size_t;
-
-		constexpr operator bool() const noexcept{
-			return true;
-		}
-
-		constexpr
-		value_type *data() noexcept{
-			return data_;
-		}
-
-		constexpr const value_type *data() const noexcept{
-			return data_;
-		}
-
-		constexpr
-		value_type &operator*() noexcept{
-			return *data_;
-		}
-
-		constexpr value_type const &operator*() const noexcept{
-			return *data_;
-		}
-
-		constexpr const value_type *operator->() const noexcept{
-			return data_;
-		}
-
-		constexpr
-		value_type *operator->() noexcept{
-			return data_;
-		}
-
-		constexpr const value_type &operator[](size_t const index) const noexcept{
-			return data_[index];
-		}
-
-		constexpr
-		value_type &operator[](size_t const index) noexcept{
-			return data_[index];
-		}
-
-		constexpr static size_type size() noexcept{
-			return Size;
-		}
-
-	private:
-		value_type data_[Size] {};
-	};
-
-	template<std::size_t Size>
-	using ByteStaticBuffer		= StaticBuffer<char, Size>;
-
-
-
 	template<std::size_t Size>
 	struct StaticMemoryResource{
 		using value_type	= void;
@@ -89,8 +31,6 @@ namespace MyBuffer{
 	private:
 		char data_[Size];
 	};
-
-
 
 } // namespace MyBuffer
 
