7733342ba3272e580f4dd4681a282e8b1594abff
Nikolay Mihaylov <nmmm@nmmm.nu>
2026-08-10 10:08:55 +0300

    add "old" documentation for PFJACCARD, PFMJACCARD, PFOVERLAP and PFMOVERLAP

--- commands_docs/commands_hll.php
+++ commands_docs/commands_hll.php
@@ -91,6 +91,89 @@ return array(
 	),
 
 	new Cmd(
+			"PFJACCARD",
+
+			"PFJACCARD / HLLJACCARD [key1] [key2]",
+
+			"Estimate jaccard similarity of two sets, e.g.<br />" .
+			"J(A, B) = |A &cap; B| / |A &cup; B|)",
+			"string (float)",
+			"estimated jaccard similarity of two sets",
+			"1.3.15.1",
+			"2 * READ",
+			false,
+			false,
+
+			"hll"
+	),
+
+	new Cmd(
+			"PFMJACCARD",
+
+			"PFMJACCARD / HLLMJACCARD [key] [key]...",
+
+			"Estimate jaccard similarity of multiple sets, e.g.<br />" .
+			"J(A, B) = |A &cap; B| / |A &cup; B|)",
+			"array",
+			"estimated jaccard similarity of two sets - first key against second, then first key against third and so on",
+			"1.3.15.1",
+			"N * READ",
+			false,
+			false,
+
+			"hll"
+	),
+
+	new Cmd(
+			"PFOVERLAP",
+
+			"PFOVERLAP / HLLOVERLAP [key1] [key2]",
+
+			"Estimate 'overlap' similarity of two sets, e.g.<br />" .
+			"Overlap(A, B) = |A &cap; B| / min(|A|, |B|)",
+			"string (float)",
+			"estimated 'overlap' similarity of two sets",
+			"1.3.15.1",
+			"2 * READ",
+			false,
+			false,
+
+			"hll"
+	),
+
+	new Cmd(
+			"PFMOVERLAP",
+
+			"PFMOVERLAP / HLLMOVERLAP [key] [key]...",
+
+			"Estimate 'overlap' similarity of multiple sets, e.g.<br />" .
+			"Overlap(A, B) = |A &cap; B| / min(|A|, |B|)",
+			"array",
+			"estimated 'overlap' similarity of two sets - first key against second, then first key against third and so on",
+			"1.3.15.1",
+			"N * READ",
+			false,
+			false,
+
+			"hll"
+	),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	new Cmd(
 			"PFMERGE",
 
 			"PFMERGE / HLLMERGE dest_key key [key]... ",
