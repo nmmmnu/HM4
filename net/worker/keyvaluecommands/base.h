2aa0fc7bb9e4a62d59f87360af3de01e56fd481c
Nikolay Mihaylov <nmmm@nmmm.nu>
2026-09-04 11:50:01 +0300

    add tests for MIndex

--- net/worker/keyvaluecommands/base.h
+++ net/worker/keyvaluecommands/base.h
@@ -253,35 +253,6 @@ namespace net::worker::commands{
 			protocol_.response_strings(buffer_, container);
 		}
 
-#if 0
-		void set_container0(){
-			std::array<std::string_view, 0> const container;
-
-			set_status_(Status::OK);
-
-			buffer_.clear();
-			protocol_.response_strings(buffer_, container);
-		}
-
-		void set_container1(std::string_view s0){
-			std::array<std::string_view, 1> const container{ s0 };
-
-			set_status_(Status::OK);
-
-			buffer_.clear();
-			protocol_.response_strings(buffer_, container);
-		}
-
-		void set_container2(std::string_view s0, std::string_view s1){
-			std::array<std::string_view, 2> const container{ s0, s1 };
-
-			set_status_(Status::OK);
-
-			buffer_.clear();
-			protocol_.response_strings(buffer_, container);
-		}
-#endif
-
 		template <typename ...Args>
 		void set_containerN(Args &&...args) {
 			static_assert((std::is_convertible_v<Args, std::string_view> && ...),
---
+++ test2/tests/MINDEX.php
@@ -0,0 +1,80 @@
+<?php
+
+function cmd_MINDEX($redis){
+	rawCommand($redis, "xndel", "a", "a");
+
+	expect("IXMADD",	rawCommand($redis, "ixmadd", "a", "01", "9999", " ", "space telescope discovers new earth sized planet"		) == 1);
+	expect("IXMADD",	rawCommand($redis, "ixmadd", "a", "02", "9998", " ", "climate summit agrees on global clean energy transition"	) == 1);
+	expect("IXMADD",	rawCommand($redis, "ixmadd", "a", "03", "9997", " ", "new battery technology doubles electric car range"	) == 1);
+	expect("IXMADD",	rawCommand($redis, "ixmadd", "a", "04", "9996", " ", "autonomous telescope rover finds ice on mars planet"	) == 1);
+	expect("IXMADD",	rawCommand($redis, "ixmadd", "a", "05", "9995", " ", "ai predicts extreme climate and clean water shortages"	) == 1);
+	expect("IXMADD",	rawCommand($redis, "ixmadd", "a", "06", "9994", " ", "scientists test nuclear battery for deep space probe"	) == 1);
+
+	$res = rawCommand($redis, "ixmsim1", "a",     "space", "", "");
+	expect("IXMSIM1",	$res === ["06", "1", "01", "1", ""]);
+
+	$res = rawCommand($redis, "ixmsim", "a", ",", "space", "", "");
+	expect("IXMSIM",	$res === ["06", "1", "01", "1", ""]);
+
+	$res = rawCommand($redis, "ixmsim", "a", ",", "space,planet", "", "");
+	expect("IXMSIM",	$res === ["01", "1", ""]);
+
+	$res = rawCommand($redis, "ixmsim", "a", ",", "climate,clean", "", "");
+	expect("IXMSIM",	$res === ["05", "1", "02", "1", ""]);
+
+	$res = rawCommand($redis, "ixmsim", "a", ",", "space,nuclear,battery", "", "");
+	expect("IXMSIM",	$res === ["06", "1", ""]);
+
+	$res = rawCommand($redis, "ixmsim", "a", ",", "space,ne", "", "");
+	expect("IXMSIM",	$res === [""]);
+
+	$res = rawCommand($redis, "ixmsim", "a", ",", "nonexistenttoken", "", "");
+	expect("IXMSIM",	$res === [""]);
+
+	expect("IXMREM",	rawCommand($redis, "ixmrem", "a", "01", "02", "03", "04", "05", "06") == 1);
+
+	// Проверка след изтриване
+	$res = rawCommand($redis, "ixmsim", "a", ",", "space", "", "");
+	expect("IXMSIM",	$res === [""]);
+
+
+
+	for ($i = 1; $i <= 15; ++$i) {
+		$id = sprintf("%02d", $i);
+		$sortKey = sprintf("%04d", 9999 - $i);
+		rawCommand($redis, "ixmadd", "a", $id, $sortKey, ",", "common,tag,news");
+	}
+
+
+
+	$p1 = rawCommand($redis, "ixmsim", "a", ",", "common,tag,news", 10, "");
+	expect("IXMSIM",	count($p1) == (10 * 2 + 1));
+	expect("IXMSIM",	$p1[0] == "15" && $p1[18] == "06");
+	$cursor = end($p1);
+	expect("IXMSIM",	$cursor == "9994~05");
+
+	$p2 = rawCommand($redis, "ixmsim", "a", ",", "common,tag,news", 10, $cursor);
+	expect("IXMSIM",	count($p2) == (5 * 2 + 1));
+	expect("IXMSIM",	$p2[0] == "05" && $p2[8] == "01");
+	$cursor = end($p2);
+	expect("IXMSIM",	$cursor === "");
+
+
+
+	$p1 = rawCommand($redis, "ixmsim1", "a", "common", 10, "");
+	expect("IXMSIM1",	count($p1) == (10 * 2 + 1));
+	expect("IXMSIM1",	$p1[0] == "15" && $p1[18] == "06");
+	$cursor = end($p1);
+	expect("IXMSIM1",	$cursor == "9994~05");
+
+	$p2 = rawCommand($redis, "ixmsim1", "a", "common", 10, $cursor);
+	expect("IXMSIM1",	count($p2) == (5 * 2 + 1));
+	expect("IXMSIM1",	$p2[0] == "05" && $p2[8] == "01");
+	$cursor = end($p2);
+	expect("IXMSIM1",	$cursor === "");
+
+
+
+	rawCommand($redis, "xndel", "a", "a");
+}
+
